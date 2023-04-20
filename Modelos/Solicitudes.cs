0f3e01b881518dfdc757fd4dd2fd33510765eb85
AlanSalgado <94387803+AlanSalgado@users.noreply.github.com>
2023-04-20 16:36:31 -0600

    Add files via upload

---
+++ Modelos/Solicitudes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Solicitudes
+    {
+        public int IdSolicitud { get; set; }
+        public string NombreCliente { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public int Lote { get; set; }
+        public int PrecioUnitario { get; set; }
+        public int Importe { get; set; }
+        public int Total { get; set; }
+        public int IdUsuario { get; set; }
+        public string Estado { get; set; }
+        public Solicitudes() { }
+    }
+}
---
+++ Modelos/Venta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos {
+    public class Venta {
+        public int FolioVenta { get; set; }
+        public string Producto { get; set; }
+        public string Cliente { get; set; }
+        public string Fecha { get; set; }
+        public int CantidadVendido { get; set; }
+        public double Subtotal { get; set; }
+        public double IVA { get; set; }
+        public double Total { get; set; }
+        public int IdUsuario { get; set; }
+
+        public Venta() { }
+    }
+}
